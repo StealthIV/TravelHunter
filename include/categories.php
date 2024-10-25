cc644439b79637ed57837c19fc56bfc5d1d8acf0
StealthIV <cferdinand500@gmail.com>
2024-10-25 18:44:45 +0800

    all goods history

--- include/categories.php
+++ include/categories.php
@@ -24,7 +24,7 @@ try {
 
     // Get the user's ID for filtering the bookings
     $id = $user['id'];
-    $profile_image = $user['image']; 
+    $profile_image = $user['image'];
     $full_name = $user['FullName'];
 
 } catch (PDOException $error) {
@@ -39,10 +39,14 @@ try {
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Travel Hunter</title>
+    <!-- CSS -->
     <link rel="stylesheet" href="../style/categories.css" />
     <link href="https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,0,0" />
+    <link rel="stylesheet" href="css/swiper-bundle.min.css" />
 </head>
+
 <body>
     <?php require_once 'nav.php'; ?>
     <section class="overlay"></section>
@@ -50,57 +54,49 @@ try {
     <section class="main">
         <div class="page-content">
             <div class="records table-responsive">
-                <div class="record-header">
-                    <div class="browse">
-                        <input type="search" placeholder="Search" class="record-search">
-                    </div>
-                </div>
-                <table id="info-table" width="100%">
+                <table id="info-table" class="table">
                     <thead>
                         <tr>
                             <th style='width: 20%;'>Name</th>
-                            <th style='width: 15%;'>Email</th>
-                            <th style='width: 15%;'>Phone</th>
-                            <th style='width: 10%;'>Number of Days</th>
-                            <th style='width: 10%;'>Booking Date</th>
-                            <th style='width: 10%;'>Package</th>
-                            <th style='width: 10%;'>Number of Guests</th>
-                            <th style='width: 10%;'>Downpayment</th>
-                            <th style='width: 10%;'>Balance</th>
-                            <th style='width: 10%;'>Payment Method</th>
-                            <th style='width: 10%;'>Reference</th>
-                            <th style='width: 10%;'>Action</th>
+                            <th style='width: 20%;'>Email</th>
+                            <th style='width: 20%;'>Phone</th>
+                            <th style='width: 20%;'>Number of Days</th>
+                            <th style='width: 20%;'>Booking Date</th>
+                            <th style='width: 20%;'>Package</th>
+                            <th style='width: 20%;'>Number of Guests</th>
+                            <th style='width: 20%;'>Total Amount</th>
+                            <th style='width: 20%;'>Payment Method</th>
+                            <th style='width: 20%;'>Reference</th>
+                            <th style='width: 20%;'>Action</th>
                         </tr>
                     </thead>
                     <tbody>
-                       
                         <?php
                         // Fetch the user's booking history from the historybookings table
-                        $pdoQuery = 'SELECT `history_id`, `id`, `name`, `email`, `phone`, `days`, `checkin`, `package`, `guests`, `downpayment`, `balance`, `Reference` 
+                        $pdoQuery = 'SELECT `history_id`, `id`, `name`, `email`, `phone`, `days`, `checkin`, `package`, `guests`, `amount`, `payment`, `Reference` 
                                      FROM `historybookings` 
                                      WHERE id = :id';
                         $pdoResult = $pdoConnect->prepare($pdoQuery);
-                        $pdoResult->execute(['id' => $user_id]);  // Filter by user_id
+                        $pdoResult->execute(['id' => $id]);  // Filter by user ID
+
                         while ($row = $pdoResult->fetch(PDO::FETCH_ASSOC)) {
-                          extract($row);
-                          echo "<tr>";
-                                echo "<td style='width: 15%;'>$name</td>";
-                                echo "<td style='width: 15%;'>$email</td>";
-                                echo "<td style='width: 15%;'>$phone</td>";
-                                echo "<td style='width: 10%;'>$days</td>";
-                                echo "<td style='width: 10%;'>$checkin</td>";
-                                echo "<td style='width: 10%;'>$package</td>";
-                                echo "<td style='width: 10%;'>$guests</td>";
-                                echo "<td style='width: 10%;'>$downpayment</td>";
-                                echo "<td style='width: 10%;'>$balance</td>";
-                                echo "<td style='width: 10%;'>$payment</td>";
-                                echo "<td style='width: 10%;'>$Reference</td>";
-                                echo "<td style='width: 10%;'>
-                                        <a href='boracaydelete.php?id=$id' onclick=\"return confirm('Are you sure you want to delete this booking?');\"><i class='bx bx-trash'></i></a>
-                                      </td>";
-                                echo "</tr>";
-                            }
-                        
+                            extract($row);
+                            echo "<tr>";
+                            echo "<td style='width: 20%;'>$name</td>";
+                            echo "<td style='width: 20%;'>$email</td>";
+                            echo "<td style='width: 20%;'>$phone</td>";
+                            echo "<td style='width: 20%;'>$days</td>";
+                            echo "<td style='width: 20%;'>$checkin</td>";
+                            echo "<td style='width: 20%;'>$package</td>";
+                            echo "<td style='width: 20%;'>$guests</td>";
+                            echo "<td style='width: 20%;'>$amount</td>";
+                            echo "<td style='width: 20%;'>$payment</td>";
+                            echo "<td style='width: 20%;'>$Reference</td>";
+                            echo "<td style='width: 20%;'>
+                                    <a href='boracaydelete.php?id=$history_id' onclick=\"return confirm('Are you sure you want to delete this booking?');\"><i class='bx bx-trash'></i></a>
+                                  </td>";
+                            echo "</tr>";
+                        }
                         ?>
                     </tbody>
                 </table>
